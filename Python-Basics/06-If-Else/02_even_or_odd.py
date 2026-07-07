790eff159b75ffd11011605dc2e49ff47b6d2ca2
Karthik-bhandarkar <karthikbhandarkar2004@gmail.com>
2026-07-07 19:36:00 +0530

    second progrm

---
+++ Python-Basics/06-If-Else/02_even_or_odd.py
@@ -0,0 +1,32 @@
+"""
+------------------------------------------------------------
+Problem No : 02
+
+Topic : If-Else
+
+Problem : Even or Odd
+
+Difficulty : Easy
+
+Concepts Used :
+- input()
+- int()
+- if
+- else
+- Modulus Operator (%)
+
+Objective :
+Write a program to check whether a number is Even or Odd.
+
+Author : Karthik Bhandarkar
+------------------------------------------------------------
+"""
+
+num = int(input("Enter a number: "))
+
+if num % 2 == 0:
+    print("Even")
+else:
+    print("odd")
+
+
