b140b3415e2bb3baef6ae47b9408c23b71a625a8
Karthik-bhandarkar <karthikbhandarkar2004@gmail.com>
2026-07-07 19:15:44 +0530

    fist program  in if else

--- Python-Basics/06-If-Else/01_if_else.py
+++ Python-Basics/06-If-Else/01_if_else.py
@@ -1,8 +1,10 @@
-# Question: Write a simple program to check if a number is positive or negative.
+# Question: Write a program to check whether a number is positive, negative, or zero.
 
-number = 10
+number = int(input("Enter a number: "))
 
-if number >= 0:
+if number > 0:
     print("The number is positive.")
+elif number == 0:
+    print("The number is zero.")
 else:
     print("The number is negative.")
