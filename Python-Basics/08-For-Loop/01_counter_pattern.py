bf71589a5ae10728e4c7ce516b6620f18c667bc8
Karthik-bhandarkar <karthikbhandarkar2004@gmail.com>
2026-07-28 17:27:14 +0530

    fir loop program 1

---
+++ Python-Basics/08-For-Loop/01_counter_pattern.py
@@ -0,0 +1,14 @@
+"""
+------------------------------------------------------------
+Problem No   : 01
+Topic        : For Loop
+Pattern      : Counter Pattern
+Difficulty   : Easy
+Concepts Used: for, range()
+Objective    : Print numbers from 1 to 10 using a for loop.
+Author       : Karthik Bhandarkar
+------------------------------------------------------------
+"""
+
+for i in range(1,11):
+    print(i)
