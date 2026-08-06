5bdf8285815dd8cbd6efc075f8cbb4b3de91d2e7
Karthik-bhandarkar <karthikbhandarkar2004@gmail.com>
2026-08-06 11:16:42 +0530

    added list 1st program

---
+++ Python-Basics/10-List/01_print_list_elements.py
@@ -0,0 +1,38 @@
+"""
+Problem: Print All List Elements
+
+Difficulty: Easy
+
+Problem Statement:
+Given a list of integers, print each element on a new line using direct traversal.
+
+Example:
+Input:
+[10, 20, 30, 40]
+
+Output:
+10
+20
+30
+40
+
+Concepts Used:
+- List
+- Direct Traversal
+- for loop
+
+Pattern:
+- Direct Traversal
+
+Time Complexity:
+O(n)
+
+Space Complexity:
+O(1)
+"""
+
+numbers = [10, 20, 30, 40]
+
+# Write your code here
+for num in numbers:
+    print(num)
